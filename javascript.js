30b35a95df4bf1c92e908041064bd0fd7c1a0dbd
Ipecac <cacem@pm.me>
2025-02-10 19:17:52 -0500

    Create grid function

--- javascript.js
+++ javascript.js
@@ -1,2 +1,34 @@
-const master = document.querySelector(".master")
+const master = document.querySelector(".master");
+//const column = document.createElement("div");
+//column.classList.add("column");
+//
+//const square = document.createElement("div");
+//column.classList.add("square");
+
+function populateCol(n) {
+  let col = document.createElement("div");
+  col.classList.add("column");
+  for (i=0;i<n;i++) {
+    let div = document.createElement("div")
+    div.classList.add("square");
+    col.appendChild(div);
+  }
+  master.appendChild(col)
+}
+
+function populateMas(n) {
+  for (x=0;x<n;x++) {
+    populateCol(n);
+  }
+}
+
+populateMas(16);
+
+let squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "blue";
+  })
+})
+
 
